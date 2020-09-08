31603c39679b4875ccd52dbbd27f81d92f6e38cb
Ananthu Kanive <ananthu.kanive@smallcase.com>
2020-09-08 16:49:09 +0530

    bridge existing android methods

--- android/src/main/java/com/smallcase/gateway/reactnative/SmallcaseGatewayModule.kt
+++ android/src/main/java/com/smallcase/gateway/reactnative/SmallcaseGatewayModule.kt
@@ -1,17 +1,119 @@
 package com.smallcase.gateway.reactnative
 
-import com.facebook.react.bridge.Callback
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+import android.util.Log
+import com.facebook.react.bridge.*
+import com.smallcase.gateway.data.SmallcaseGatewayListeners
+import com.smallcase.gateway.data.listeners.DataListener
+import com.smallcase.gateway.data.listeners.TransactionResponseListener
+import com.smallcase.gateway.data.models.Environment
+import com.smallcase.gateway.data.models.InitialisationResponse
+import com.smallcase.gateway.data.models.TransactionResult
+import com.smallcase.gateway.data.requests.InitRequest
+import com.smallcase.gateway.portal.SmallcaseGatewaySdk
+import com.smallcase.gateway.screens.transaction.activity.TransactionProcessActivity
+
 
 class SmallcaseGatewayModule(reactContext: ReactApplicationContext?) : ReactContextBaseJavaModule(reactContext!!) {
+    companion object {
+        const val TAG = "SmallcaseGatewayModule"
+    }
+
     override fun getName(): String {
         return "SmallcaseGateway"
     }
 
     @ReactMethod
-    fun sampleMethod(stringArgument: String, numberArgument: Int, callback: Callback) {
-        callback.invoke("Received numberArgument: $numberArgument stringArgument: $stringArgument")
+    fun setConfigEnvironment(
+            envName: String,
+            gateway: String,
+            isLeprachaunActive: Boolean,
+            preProvidedBrokers: ReadableArray,
+            promise: Promise) {
+        Log.d(TAG, "setConfigEnvironment: start")
+        try {
+            val brokerList = ArrayList<String>()
+            for (index in 0 until preProvidedBrokers.size()) {
+                val broker = preProvidedBrokers.getString(index)
+                if (broker != null) {
+                    brokerList.add(broker)
+                }
+            }
+
+            val protocol = getProtocol(envName)
+
+            val env = Environment(protocol, gateway, isLeprachaunActive, brokerList)
+
+            SmallcaseGatewaySdk.setConfigEnvironment(env, object : SmallcaseGatewayListeners {
+                override fun onGatewaySetupSuccessfull() {
+                    promise.resolve(true)
+                }
+
+                override fun onGatewaySetupFailed(error: String) {
+                    promise.reject(Throwable(error))
+                }
+            })
+        } catch (e: Exception) {
+            promise.reject(e)
+        }
+    }
+
+    @ReactMethod
+    fun init(sdkToken: String, promise: Promise) {
+        Log.d(TAG, "init: start")
+
+        val initReq = InitRequest(sdkToken)
+        SmallcaseGatewaySdk.init(initReq, object : DataListener<InitialisationResponse> {
+            override fun onFailure(errorCode: Int, errorMessage: String) {
+                promise.reject(Throwable(errorMessage))
+            }
+
+            override fun onSuccess(response: InitialisationResponse) {
+                Log.d(TAG, "init success: $response");
+                promise.resolve(true)
+            }
+
+        })
+    }
+
+    @ReactMethod
+    fun triggerTransaction(transactionId: String, promise: Promise) {
+        Log.d(TAG, "triggerTransaction: start")
+        val activity = currentActivity;
+        if (activity != null) {
+            SmallcaseGatewaySdk.triggerTransaction(activity, transactionId, object : TransactionResponseListener {
+                override fun onSuccess(transactionResult: TransactionResult) {
+                    val res = resultToWritableMap(transactionResult)
+                    promise.resolve(res)
+                }
+
+                override fun onError(errorCode: Int, errorMessage: String) {
+                    promise.reject(Throwable(errorMessage))
+                }
+            })
+        } else {
+            promise.reject(Throwable("no activity"))
+        }
+    }
+
+    private fun getProtocol(envName: String): Environment.PROTOCOL {
+        return when (envName) {
+            "production" -> Environment.PROTOCOL.PRODUCTION
+            "development" -> Environment.PROTOCOL.DEVELOPMENT
+            "staging" -> Environment.PROTOCOL.STAGING
+            else -> Environment.PROTOCOL.PRODUCTION
+        }
+    }
+
+    private fun resultToWritableMap(result : TransactionResult) : WritableMap {
+        val writableMap: WritableMap = Arguments.createMap()
+
+        writableMap.putString("data", result.data)
+        writableMap.putBoolean("success", result.success)
+        writableMap.putString("error", result.error)
+        result.errorCode?.let {
+            writableMap.putInt("errorCode", it)
+        }
+        writableMap.putString("transaction", result.transaction.name)
+        return writableMap
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 import { NativeModules } from "react-native";
 
-const { SmallcaseGateway } = NativeModules;
+const { SmallcaseGateway: SmallcaseGatewayNative } = NativeModules;
+
+/**
+ *
+ * @typedef {Object} envConfig
+ * @property {string}        gatewayName     - unique name on consumer
+ * @property {boolean}       isLeprechaun    - leprechaun mode toggle
+ * @property {Array<string>} brokerList      - list of broker names
+ * @property {'production' | 'staging' | 'development'}  environmentName - environment name
+ *
+ * @typedef {Object} transactionRes
+ * @property {string}   data        - response data
+ * @property {boolean}  success     - success flag
+ * @property {Number}   [errorCode] - error code
+ * @property {string}   transaction - transaction name
+ */
+
+const ENV = {
+  STAG: " staging",
+  DEV: "development",
+  PROD: "production",
+};
+
+/**
+ * configure the sdk with
+ * @param {envConfig} envConfig
+ */
+const setConfigEnvironment = async (envConfig) => {
+  const { brokerList, gatewayName, isLeprechaun, environmentName } = envConfig;
+
+  await SmallcaseGatewayNative.setConfigEnvironment(
+    environmentName,
+    gatewayName,
+    isLeprechaun,
+    brokerList
+  );
+};
+
+/**
+ * initialize sdk with a session
+ *
+ * note: this must be called after `setConfigEnvironment()`
+ * @param {string} sdkToken
+ */
+const init = async (sdkToken) => {
+  await SmallcaseGatewayNative.init(sdkToken);
+};
+
+/**
+ * triggers a transaction with a transaction id
+ *
+ * @param {string} transactionId
+ * @returns {Promise<transactionRes>}
+ */
+const triggerTransaction = async (transactionId) => {
+  return SmallcaseGateway.triggerTransaction(transactionId);
+};
+
+const SmallcaseGateway = {
+  ENV,
+  init,
+  triggerTransaction,
+  setConfigEnvironment,
+};
 
 export default SmallcaseGateway;
