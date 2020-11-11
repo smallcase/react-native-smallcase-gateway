7917eaa92c6b98e5bf968d0bb70fb83806986e89
Ananthu Kanive <ananthu.kanive@smallcase.com>
2020-11-11 15:20:45 +0530

    chore: modify git hook script

--- init-githooks.sh
+++ init-githooks.sh
@@ -1,7 +1,4 @@
 cd .git/hooks
 
-echo "#!/bin/bash\nexec < /dev/tty && node_modules/.bin/cz --hook || true" >prepare-commit-msg
-chmod u+x prepare-commit-msg
-
 echo "#!/bin/bash\ncat \$1 | node_modules/.bin/commitlint" >commit-msg
 chmod u+x commit-msg
