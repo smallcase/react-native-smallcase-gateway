479109d9d491a853f1d09744fe2f36fffb1409dc
Indrajit Roy <70997867+indrajit-roy@users.noreply.github.com>
2022-07-14 09:48:45 +0530

    fix: version not null

--- src/SmallcaseGateway.js
+++ src/SmallcaseGateway.js
@@ -41,7 +41,7 @@ let defaultBrokerList = [];
 const setConfigEnvironment = async (envConfig) => {
   const safeConfig = safeObject(envConfig);
 
-  await SmallcaseGatewayNative.setHybridSdkVersion(null);
+  await SmallcaseGatewayNative.setHybridSdkVersion(version);
 
   const {
     brokerList,
