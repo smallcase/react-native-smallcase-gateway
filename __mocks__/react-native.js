d85689eac4324b8c877a502d539aee7317eb7715
Ananthu Kanive <ananthu.kanive@smallcase.com>
2022-08-09 14:18:21 +0530

    feat: delete old files

--- __mocks__/react-native.js
+++
@@ -1,8 +0,0 @@
-export const NativeModules = {
-  SmallcaseGateway: {
-    init: jest.fn(),
-    triggerLeadGen: jest.fn(),
-    triggerTransaction: jest.fn(),
-    setConfigEnvironment: jest.fn(),
-  },
-};
