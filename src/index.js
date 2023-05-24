33a1d7504d01dc06b56d1e2c92aa543b28afd046
Indrajit Roy <70997867+indrajit-roy@users.noreply.github.com>
2023-05-24 17:12:34 +0530

    fix: export ScLoan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import SmallcaseGateway from "./SmallcaseGateway";
+import ScLoan from "./ScLoan";
 import { ENV, TRANSACTION_TYPE, ERROR_MSG } from "./constants";
 
+export { ScLoan }
 export default { ...SmallcaseGateway, ENV, ERROR_MSG, TRANSACTION_TYPE };
