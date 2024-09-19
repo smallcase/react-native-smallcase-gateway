8e3a2b6409df91d9af633b6e95369f4d1ba43161
aaditya singh <aaditya.singh@smallcase.com>
2024-09-19 13:08:32 +0530

    fix: sdk linked with project

--- gw-mob-smartinvesting-react-native/metro.config.js
+++ gw-mob-smartinvesting-react-native/metro.config.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Define the root path of the project
+const root = path.resolve(__dirname, '..');
+
+// Metro configuration
+module.exports = mergeConfig(getDefaultConfig(__dirname), {
+  resolver: {
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+  watchFolders: [
+    path.resolve(__dirname, '..'), // Watch the directory where your library is located
+  ],
+});
