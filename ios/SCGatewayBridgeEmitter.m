75bd9c3f85786e04fb8d025df98a2487a0b336ca
Dhruv Porwal <dhruv.porwal@smallcase.com>
2025-07-30 02:37:13 +0530

    feat(analytics): Adding events capturing through new RCTBridgeModule

--- ios/SCGatewayBridgeEmitter.m
+++ ios/SCGatewayBridgeEmitter.m
@@ -7,9 +7,9 @@
 //
 
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_REMAP_MODULE(SCGatewayBridgeEmitter, SCGatewayBridgeEmitter, NSObject)
-
+@interface RCT_EXTERN_REMAP_MODULE(SCGatewayBridgeEmitter, SCGatewayBridgeEmitter, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(startListening:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
